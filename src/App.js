a147f5fc8f70677c7178ccb7327427655533a054
Alexander <sumry.stereo123@ukr.net>
2023-01-10 22:10:39 +0200

    add PostFilter.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { useState} from "react";
+import React, {useMemo, useState} from "react";
 import './styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
-import Counter from "./components/Counter";
+import PostFilter from "./components/PostFilter";
 
 function App() {
   const[posts, setPosts] = useState([
@@ -12,7 +11,18 @@ function App() {
     {id:3, title:"C++", body:"333"},
   ]);
 
-  const [selectedSort, setSelectedSort] = useState('')
+  const [filter, setFilter] = useState({sort:'', query:''})
+
+  const sortedPosts = useMemo(() => {
+      if(filter.sort){
+          return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+      }
+      return posts;
+  },[filter.sort, posts])
+
+  const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+    },[filter.query, sortedPosts])
 
   const createPost = (newPost) => {
   setPosts([...posts, newPost])
@@ -21,38 +31,16 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-  const sortPosts = (sort) => {
-      setSelectedSort(sort);
-      setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
-
-  }
-
   return (
     <div className="App">
         <PostForm create={createPost}/>
-      <hr style={{margin:'15px 0'}}/>
-      <div>
-          <MySelect
-              value={selectedSort}
-              onChange={sortPosts}
-              defaultValue="Select"
-              options={[
-                    {value: 'title', name:'Name'},
-                    {value: 'body', name:'Description'},
-            ]}
-          />
-      </div>
-      {posts.length
-          ?
-          <PostList remove={removePost} posts={posts} title="JS list"/>
-          :
-          <h1 style={{textAlign: 'center'}}>
-            Posts not find
-          </h1>
-      }
-            <Counter/>
+            <hr style={{margin:'15px 0'}}/>
+                <PostFilter
+                    filter={filter}
+                    setFilter={setFilter}
+                />
+                    <PostList remove={removePost} posts={sortedAndSearchedPosts} title="JS list"/>
     </div>
-
   );
 }
 
