5a5b6286be24ed5e24351bdc9216e4eaa5682e4d
Alexander <sumry.stereo123@ukr.net>
2023-01-11 16:57:58 +0200

    add MyModal.jsx, MyModal.module.css

--- src/App.js
+++ src/App.js
@@ -3,45 +3,54 @@ import './styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
-  const[posts, setPosts] = useState([
-    {id:1, title:"Javascript", body:"222"},
-    {id:2, title:"Python", body:"111"},
-    {id:3, title:"C++", body:"333"},
-  ]);
+    const [posts, setPosts] = useState([
+        {id: 1, title: "Javascript", body: "222"},
+        {id: 2, title: "Python", body: "111"},
+        {id: 3, title: "C++", body: "333"},
+    ]);
 
-  const [filter, setFilter] = useState({sort:'', query:''})
+    const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false)
 
-  const sortedPosts = useMemo(() => {
-      if(filter.sort){
-          return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-      }
-      return posts;
-  },[filter.sort, posts])
+    const sortedPosts = useMemo(() => {
+        if (filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+        }
+        return posts;
+    }, [filter.sort, posts])
 
-  const sortedAndSearchedPosts = useMemo(() => {
+    const sortedAndSearchedPosts = useMemo(() => {
         return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-    },[filter.query, sortedPosts])
+    }, [filter.query, sortedPosts])
 
-  const createPost = (newPost) => {
-  setPosts([...posts, newPost])
-}
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+        setModal(false)
+    }
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
 
-  return (
-    <div className="App">
-        <PostForm create={createPost}/>
-            <hr style={{margin:'15px 0'}}/>
-                <PostFilter
-                    filter={filter}
-                    setFilter={setFilter}
-                />
-                    <PostList remove={removePost} posts={sortedAndSearchedPosts} title="JS list"/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+                Create post
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
+            <hr style={{margin: '15px 0'}}/>
+            <PostFilter
+                filter={filter}
+                setFilter={setFilter}
+            />
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="JS list"/>
+        </div>
+    );
 }
 
 export default App;
